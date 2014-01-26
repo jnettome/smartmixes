77b370874a37372b4ac72d887f15fcf056424554
João Netto <hi@joaonetto.me>
2014-01-26 21:48:45 -0200

    Adding tests to gameserver model

--- test/models/gameserver_test.rb
+++ test/models/gameserver_test.rb
@@ -3,12 +3,19 @@ require_relative '../../models/gameserver'
 
 describe Gameserver do
   before do
-    @server = Gameserver.new
+    @server = Gameserver.new(ip: '127.0.0.1')
   end
 
   describe "checking if server is alive" do
     it "respond with true" do
       assert_equal true, @server.alive?
+    end
+  end
+
+  describe "returning records" do
+    it "respond to all" do
+
+      assert_equal Array, Gameserver.all.class
     end
   end
 end
